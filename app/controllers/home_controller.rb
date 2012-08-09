b7e3894ba7c4c7b4fc4414debd7dc9526811026e
Rob Keefer <rob@pomiet.com>
2012-08-09 19:52:32 -0400

    Gibbons Initial

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def about_us
+  end
+
+  def contact_us
+  end
+
+  def terms_conditions
+  end  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gibbons::Application.config.secret_token = '9a9d16fc9652d9fce022fbd3fb3e90d3ad2e6f90bfd59e3ab6d92374d95227673e3f6f66a22863c843d64411703775431f09b8f86b51f9478021b92eb97c890e'
